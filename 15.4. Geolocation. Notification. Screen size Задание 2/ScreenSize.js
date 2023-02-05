1e54a2c54d20711b5ced434fcba025ebf0810ef4
Dmitriy <114744105+Dmitriy-Vostrikov@users.noreply.github.com>
2023-02-05 14:43:00 +0300

    Add files via upload

---
+++ 15.4. Geolocation. Notification. Screen size Задание 2/ScreenSize.js
@@ -0,0 +1,9 @@
+//function getResolution() {
+//   alert("Ваш размер экрана: " + screen.width + " на " + screen.height);
+//}
+
+function getAvailSize() {
+    alert("Доступная ширина и высота экрана: " + screen.availWidth + " на " + screen.availHeight);
+}
+
+// в задании только не понял какие именно размеры нужны, чистая высота и ширина экрана или за исключением панели задач windows, поэтому приложил два варианта.
